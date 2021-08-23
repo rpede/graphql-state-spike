7461f079291cf7d8c619bbf8ef735a41ad8c3d11
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2021-08-23 15:58:12 +0200

    Add material and graphql to ui

--- apps/postr/src/app/app.component.ts
+++ apps/postr/src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@graphql-state-spike/api-interfaces';
 
 @Component({
-  selector: 'graphql-state-spike-root',
+  selector: 'postr-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
 }
--- apps/postr/src/app/app.module.ts
+++ apps/postr/src/app/app.module.ts
@@ -3,10 +3,53 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GraphQLModule } from './graphql.module';
+
+const routes: Routes = [
+  {
+    component: PostsComponent,
+    path: 'posts'
+  },
+  {
+    component: ProfileComponent,
+    path: 'profile'
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, NavigationComponent, PostsComponent, ProfileComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, { enableTracing: true }),
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    GraphQLModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

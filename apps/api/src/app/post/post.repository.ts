4e59470cde12a331a979c5928f432167d3bec2d6
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2021-08-23 14:11:49 +0200

    Add some test data

--- apps/api/src/app/post/post.repository.ts
+++ apps/api/src/app/post/post.repository.ts
@@ -2,6 +2,28 @@ import { BaseRepository } from '../base-repository';
 import {Post} from '../graphql';
 
 export class PostRepository extends BaseRepository<Post & {authorId: number}> {
+  constructor() {
+    super();
+    this.add({
+      id: 0,
+      title: 'Hello world',
+      authorId: 0,
+      author: null
+    });
+    this.add({
+      id: 1,
+      title: 'Another amazing post',
+      authorId: 0,
+      author: null
+    });
+    this.add({
+      id: 2,
+      title: 'Ich habe kein geld',
+      authorId: 1,
+      author: null
+    });
+  }
+
   findByAuthor(authorId: number) {
     return this.entities.filter((post) => post.authorId === authorId);
   }

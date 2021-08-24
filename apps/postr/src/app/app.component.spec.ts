218cda77caa6f9b5421fb3ea171d76a67d55aa5a
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2021-08-24 21:11:52 +0200

    Example of a stub component

--- apps/postr/src/app/app.component.spec.ts
+++ apps/postr/src/app/app.component.spec.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+@Component({ selector: 'postr-navigation' })
+class StubNavigationComponent {}
+
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      imports: [HttpClientModule],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [StubNavigationComponent, AppComponent],
+      }).compileComponents();
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);

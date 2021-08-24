58719fbfe2b90dfc2f51c086d4d4adcaecafd5f5
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2021-08-24 22:01:53 +0200

    Navigation spec

--- apps/postr/src/app/navigation/navigation.component.spec.ts
+++ apps/postr/src/app/navigation/navigation.component.spec.ts
@@ -1,32 +1,39 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
 import { LayoutModule } from '@angular/cdk/layout';
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 import { NavigationComponent } from './navigation.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { menu } from './menu';
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [NavigationComponent],
-      imports: [
-        NoopAnimationsModule,
-        LayoutModule,
-        MatButtonModule,
-        MatIconModule,
-        MatListModule,
-        MatSidenavModule,
-        MatToolbarModule,
-      ]
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [NavigationComponent],
+        imports: [
+          NoopAnimationsModule,
+          LayoutModule,
+          MatButtonModule,
+          MatIconModule,
+          MatListModule,
+          MatSidenavModule,
+          MatToolbarModule,
+          RouterTestingModule,
+        ],
+        providers: [{ provide: MATERIAL_SANITY_CHECKS, useValue: false }],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavigationComponent);
@@ -36,5 +43,11 @@ describe('NavigationComponent', () => {
 
   it('should compile', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should list menu items', () => {
+    const menuItems =
+      fixture.nativeElement.querySelectorAll('a[mat-list-item]');
+    expect(menuItems).toHaveLength(menu.length);
   });
 });

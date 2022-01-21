79605eceba9af27b17044da6c1f6d3c605142309
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2022-01-21 16:47:26 +0100

    Fix warning in spec

--- apps/postr/src/app/post-list/post-list.component.spec.ts
+++ apps/postr/src/app/post-list/post-list.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostListComponent } from './post-list.component';
 import { ApolloTestingModule } from 'apollo-angular/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('PostsComponent', () => {
   let component: PostListComponent;
@@ -9,7 +10,7 @@ describe('PostsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ApolloTestingModule],
+      imports: [ApolloTestingModule, RouterTestingModule],
       declarations: [ PostListComponent ]
     })
     .compileComponents();
